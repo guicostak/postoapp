b4f83a60ccdd1275c2535cc5950ac1a8e2bf4a74
Guilherme Augustto <guilhermecosta.barros@gmail.com>
2023-08-20 23:52:10 -0300

    criando rep

---
+++ src/main/java/com/example/postoapp/PostoappApplication.java
@@ -0,0 +1,13 @@
+package com.example.postoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostoappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/postoapp/PostoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
